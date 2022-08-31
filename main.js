50b966db91a089694fbe864a153d2ed6fa1a061c
Rohit Sharma <codingscenes@gmail.com>
2022-08-31 18:32:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-// functions
-function add10Fn  (n) {
+// functions required for the compose
+function add10Fn(n) {
   return n + 10;
 }
 
 function doubleFn(n) {
-  return  n * n;
+  return n * n;
 }
 
 function sub10Fn(n) {
- return n - 10;
+  return n - 10;
 }
 
 function divide10Fn(n) {
   return n / 10;
 }
 
-
 // compose . reduce (closure)
 // previousValue => divide10Fn, currentValue => sub10Fn
-function composeFn (fn1, fn2) { // nested => closure (fn args => inner fns)
+function composeFn(fn1, fn2) {
+  // nested => closure (fn args => inner fns)
   return function (n) {
     return fn1(fn2(n)); // divide10Fn(sub10Fn(10))
-  }
+  };
 }
 
 function PipeFn(fn1, fn2) {
   // nested => closure (fn args => inner fns)
-  return function (...nums) { // [10,20]
+  return function (...nums) {
+    // [10,20]
     return fn2(fn1(...nums)); // divide10Fn(sub10Fn(10)) 10,20
   };
 }
 
 function numberFactory(...fns) {
-
-  return fns.reduce(composeFn);//reducer, initialValue
+  return fns.reduce(composeFn); //reducer, initialValue
 }
 
-const output = numberFactory(divide10Fn, sub10Fn, doubleFn, add10Fn)(10,20)
+const output = numberFactory(divide10Fn, sub10Fn, doubleFn, add10Fn)(10, 20);
 
-console.log(output)
+console.log(output);
 
 // flipkart user
 const users = {
@@ -46,13 +46,10 @@ const users = {
   wallet: 3000,
   order: [],
   cart: [],
-  purchased: []
-}
+  purchased: [],
+};
 // add to wallet
 // add to order//
 // add to cart..
 /// purchase the order in the cart//
 // emtpy cart
-
-
-
