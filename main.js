acc6c399249211c0dff4fe39e6172e65a370fb4f
Rohit Sharma <codingscenes@gmail.com>
2022-09-01 14:51:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,21 @@
+//1 - download video
+// 2- process video
+// asynchronous callback
 
-const arr = [2, 3, 4, 5, 6, 7, 8];
-
-const callbackFnEven = function(element, index, array)  {
-    return element % 2 == 0;
+// callback queue & Event-loop (async)
+function downloadVideo(file, callback) {
+    console.log(`Video downloading [${file}]`);
+     setTimeout(() => {
+         console.log(`Video downloading completed [${file}]`)
+        callback(file);
+     }, 2000);
 }
 
-const callbackFnOdd = function (element, index, array) {
-  return element % 2 !== 0;
-};
-
-const outputEven = arr.filter(callbackFnEven);
-const outputOdd = arr.filter(callbackFnOdd);
-
-// console.log(outputEven, outputOdd);
-
-// my filter
-function filter(numbers, callback) {
-    const tempArr = [];
-    let i = 0;
-    for (let number of numbers) {
-        if (callback(number, i, numbers )) { // true number % 2== 0
-            tempArr.push(number)
-        }
-        i++;
-    }
-    return tempArr;
+function processDownloadedVideo(videoFile) {
+    console.log(`Processing download video ${videoFile}`);
 }
 
-const outputEvenNums = filter(arr, callbackFnEven);
-const outputOddNums = filter(arr, callbackFnOdd);
-
-console.log(outputEvenNums, outputOddNums);
-
-
-const callbackMap = function (element, index, array) {
-    return element * 5;
-}
-
-// my map
-function map(numbers, callback) {
-    const tempArr = [];
-    let i = 0;
-    for (let number of numbers) {
-        const res = callback(number, i, numbers);
-        tempArr.push(res);
-
-        i++;
-    }
-    return tempArr;
-}
-
-const myMapOutput = map(arr, callbackMap);
-console.log(myMapOutput)
+// time taking => async
+downloadVideo('CodingScenes.mp4', function (downloadedFile) {
+    processDownloadedVideo(downloadedFile); // wait till file gets downloaded
+});
