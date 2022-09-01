e3913052ada9bfb2bec29e86db502189178468b6
Rohit Sharma <codingscenes@gmail.com>
2022-09-02 01:45:22 +0530

    Promises

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
-//1 - download video
-// 2- process video
-// asynchronous callback
+// const promise = new Promise((resolve, reject) => {
+//     resolve(25251235);
+//     resolve(54433333);
+// });
 
-// callback queue & Event-loop (async)
-function downloadVideo(file, callback) {
-    console.log(`Video downloading [${file}]`);
-     setTimeout(() => {
-         console.log(`Video downloading completed [${file}]`)
-        callback(file);
-     }, 2000);
+// // continuation passing style
+// function add(x, y, cb) {
+//     cb(x + y);
+//     console.log('This is after cb ran!!')
+
+// }
+//  add(3,5, function(value) {
+//     console.log('value', value);
+// })
+
+
+// // Handle exceptions in the code
+// promise.then((value) => {// promise resolved
+//     console.log('value: ', value);
+// }).catch(error => {
+
+//     console.log('error: ', error);
+
+// }).finally(() => {
+//     console.log('masti mein finally!')
+// })
+
+
+// using built in fetch api (promise)
+
+const getTodo = (id) => {
+    const url = `https://jsonplaceholder.cypress.io/todos333/${id}`;
+    // fetch api does not reject if errors are 4xx or 5xx
+    fetch(url).then(response => {
+        if (response.ok) {
+          return response.json(); // Promise
+        }
+        return Promise.reject('API failed with error!');
+
+    }).then(jsonResponse => console.log(jsonResponse))
+        .catch(error => {// safely error handle
+            console.log('error', error);
+            return Promise.resolve('This is ok!!');
+        })
+        .then((finalMsg) => {
+        console.log(finalMsg)
+    })
 }
 
-function processDownloadedVideo(videoFile) {
-    console.log(`Processing download video ${videoFile}`);
-}
-
-// time taking => async
-downloadVideo('CodingScenes.mp4', function (downloadedFile) {
-    processDownloadedVideo(downloadedFile); // wait till file gets downloaded
-});
+getTodo(2);
+getTodo(2);
+getTodo(2);
