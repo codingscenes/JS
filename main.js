67223b916b25a76caf911bedc5c4393a5a5b1913
Rohit Sharma <codingscenes@gmail.com>
2022-09-05 12:33:02 +0530

    ...

--- main.js
+++ main.js
@@ -1,68 +1,28 @@
-const fillHtml = (data) => {
-  let htmlData = null;
-  if (typeof data === 'object' && data.hasOwnProperty('length')) {
-    htmlData = data;
-  } else {
-    htmlData = [data];
-  }
-  htmlData.forEach((eachOne, index) => {
-    if (eachOne.hasOwnProperty('status')) {
-      const box = document.getElementById(`id${index + 1}`);
-      const statusElem = box.getElementsByClassName('status')[0];
-      statusElem.textContent = eachOne.status;
-    } else {
-      const box = document.getElementById(`id${index + 1}`);
-      const idElem = box.getElementsByClassName('userId')[0];
-      const titleElem = box.getElementsByClassName('title')[0];
-      const completedElem = box.getElementsByClassName('completed')[0];
-      idElem.textContent = eachOne.id;
-      titleElem.textContent = eachOne.title;
-      completedElem.textContent = eachOne.completed
-        ? 'Good Job'
-        : 'Keep trying';
-    }
-  });
-};
+//Guess the output 
+console.log(1);
+function printHello() {
+  console.log('Hello');
+}
 
-const fillCatch = (err) => {
-  document.getElementById('error').textContent = 'ERROR';
-};
+function cb1() {
+  console.log(2);
+}
+function cb2() {
+  console.log(3);
+}
+function cb3(value) {
+  console.log(value);
+  return 8;
+}
+function cb4(value) {
+  console.log(value);
+}
 
-const delay = (ms) => new Promise((r) => setTimeout(r, ms));
+setTimeout(cb1, 0);
 
-const fetchToDo = (url, timeMs = 0) => {
-  return delay(timeMs)
-    .then(() => {
-      return fetch(url);
-    })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject('An error occurred. Please try again!');
-    });
-};
+setTimeout(cb2, 100);
 
-const url1 = `https://jsonplaceholder.cypress.io/todos/1`;
-const url2 = `https://jsonplaceholder.cypress.io/todos/2`;
-const url3 = `https://jsonplaceholder.cypress.io/todos/3`;
-const url4 = `https://jsonplaceholder.cypress.io/todos/4`;
+Promise.resolve(4).then(cb3).then(cb4);
 
-const p1 = fetchToDo(url1);
-const p2 = fetchToDo(url2);
-const p3 = fetchToDo(url3);
-const p4 = fetchToDo(url4);
-
-// Using Promise static //.all.any
-
-// .allSettled()
-const resultPromise = Promise.all([p1, p2, p3, p4])
-// extracting data from result promise
-resultPromise.then((response) => {
-  console.log('response', response);
-  fillHtml(response)
-}).catch((error) => {
-  console.log('error', error);
-  fillCatch(error)
-})
-
+printHello();
+console.log(5);
