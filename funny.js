d9158d8bc9f1c246697054163564bb9069cc45c7
Rohit Sharma <codingscenes@gmail.com>
2022-09-13 23:43:37 +0530

    modified

--- funny.js
+++ funny.js
@@ -1,23 +1,26 @@
-const startBtn = document.getElementById('active-btn');
-const stopBtn = document.getElementById('pause-btn');
+(function () {
+  const startBtn = document.getElementById('active-btn');
+  const stopBtn = document.getElementById('pause-btn');
 
-const activeContainer = document.getElementById('active');
-const pauseContainer = document.getElementById('pause');
+  const activeContainer = document.getElementById('active');
+  const pauseContainer = document.getElementById('pause');
 
-startBtn.addEventListener('click', function () {
-  activateAnimation();
-});
+  startBtn.addEventListener('click', function () {
+    activateAnimation();
+  });
 
-stopBtn.addEventListener('click', function () {
-  deactivateAnimation();
-});
+  stopBtn.addEventListener('click', function () {
+    deactivateAnimation();
+  });
 
-function activateAnimation() {
-  activeContainer.style.display = 'flex';
-  pauseContainer.style.display = 'none';
-}
+  function activateAnimation() {
+    activeContainer.style.display = 'flex';
+    pauseContainer.style.display = 'none';
+  }
 
-function deactivateAnimation() {
-  activeContainer.style.display = 'none';
-  pauseContainer.style.display = 'flex';
-}
+  function deactivateAnimation() {
+    activeContainer.style.display = 'none';
+    pauseContainer.style.display = 'flex';
+  }
+
+})()
--- main.js
+++ main.js
@@ -1,51 +1,54 @@
-const numElement = document.getElementById('bigNumber');
-let timer = null;
-let btnTimerStart = 'Start';
-let btnTimerResume = 'Resume';
-let btnTimerStop = 'Stop';
-let timerBtnState = btnTimerStop;
+(function () {
+  const numElement = document.getElementById('bigNumber');
+  let timer = null;
+  const btnTimerStart = 'Start';
+  const btnTimerResume = 'Resume';
+  const btnTimerStop = 'Stop';
+  let timerBtnState = btnTimerStop;
 
-let timerCount = 0;
-// for increment
-document
-  .getElementById('startCount')
-  .addEventListener('click', function (event) {
-    if (timerBtnState == btnTimerStop) {
-      event.target.textContent = btnTimerStop;
-      timerBtnState = btnTimerStart;
-      startTimer(false);
-    } else if (timerBtnState == btnTimerStart) {
-      event.target.textContent = btnTimerResume;
-      timerBtnState = btnTimerResume;
-      timerCount = +numElement.textContent || 0;
-      clearInterval(timer);
-    } else if (timerBtnState == btnTimerResume) {
-      timerBtnState = btnTimerStart;
-      event.target.textContent = btnTimerStop;
-      numElement.textContent = +timerCount;
-      timerCount = null;
-      startTimer(false);
-    }
+  let timerCount = 0;
+  //! for increment
+  document
+    .getElementById('startCount')
+    .addEventListener('click', function (event) {
+      if (timerBtnState == btnTimerStop) {
+        event.target.textContent = btnTimerStop;
+        timerBtnState = btnTimerStart;
+        startTimer(false);
+      } else if (timerBtnState == btnTimerStart) {
+        event.target.textContent = btnTimerResume;
+        timerBtnState = btnTimerResume;
+        timerCount = +numElement.textContent || 0;
+        clearInterval(timer);
+      } else if (timerBtnState == btnTimerResume) {
+        timerBtnState = btnTimerStart;
+        event.target.textContent = btnTimerStop;
+        numElement.textContent = +timerCount;
+        timerCount = null;
+        startTimer(false);
+      }
+    });
+
+  document.getElementById('stopCount').addEventListener('click', function () {
+    timerCount = null;
+    timerBtnState = btnTimerStop;
+    const btnStartCountElem = document.getElementById('startCount');
+    clearInterval(timer);
+    btnStartCountElem.innerText = btnTimerStart;
+    numElement.textContent = 0;
   });
 
-document.getElementById('stopCount').addEventListener('click', function () {
-  timerCount = null;
-  timerBtnState = btnTimerStop;
-  const btnStartCountElem = document.getElementById('startCount');
-  clearInterval(timer);
-  btnStartCountElem.innerText = btnTimerStart;
-  numElement.textContent = 0;
-});
-
-function startTimer(reset) {
-  if (reset && timer) {
-    numElement.textContent = 0;
-    clearInterval(timer);
-  } else {
-    clearInterval(timer);
+  function startTimer(reset) {
+    if (reset && timer) {
+      numElement.textContent = 0;
+      clearInterval(timer);
+    } else {
+      clearInterval(timer);
+    }
+    timer = setInterval(() => {
+      let prevNum = timerCount || +numElement.textContent;
+      numElement.textContent = prevNum + 1;
+    }, 1000);
   }
-  timer = setInterval(() => {
-    let prevNum = timerCount || +numElement.textContent;
-    numElement.textContent = prevNum + 1;
-  }, 1000);
-}
+
+})()
